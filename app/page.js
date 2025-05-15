bd0aef5950da30b8b81f131d5e355e7cde3d907a
ezifydevelopers <dev@gforcesecurity.ca>
2025-05-16 00:25:28 +0500

    feat: site main header

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Header from './components/header';
 
 export default function Home() {
   return (
-    <div></div>
+      <div>
+          <Header />
+      </div>
   );
 }
